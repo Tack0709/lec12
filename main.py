62328d86da5c14313ccc718cb191b79bfd1e4a35
Takuma Mizuno <34719035ict.nitech.ac.jp>
2024-07-18 07:22:24 +0000

    change main double caluculate

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import argparse
 from recursive import fibonacci
 from recursive import factorial
 
+
 def main():
-    parser = argparse.ArgumentParser(description = 'factorial or fibonacci one number')
+    parser = argparse.ArgumentParser(
+        description='factorial or fibonacci one number')
     parser.add_argument('arg1', type=int,
                         help='first argment')
-    parser.add_argument('arg3', choices=['factorial', 'fibonacci'],
-                        help='factorial or fibonacci (default: fibonacci)')
-    
+
     args = parser.parse_args()
     a = args.arg1
-    b = args.arg2
 
-    print(a,b)
+    print(a)
 
-    print(factorial(5))
+    print(factorial(a))
+    print(fibonacci(a))
 
 
 if __name__ == '__main__':
