d68af0cd46620684458a514767b0e7caec2b807f
Takuma Mizuno <34719035ict.nitech.ac.jp>
2024-07-18 07:31:42 +0000

    add document

--- recursive.py
+++ recursive.py
@@ -1,9 +1,18 @@
+# @fn factorial(n)
+#  @brief nからの階乗を計算する
+#  @param   n (int): 1つ目の引数
+#  @return   int: n*factorial(n-1), n==0, n==1のとき1
 def factorial(n):
     if n == 0 or n == 1:
         return 1
     else:
         return n * factorial(n - 1)
- 
+
+    # @fn fibonacci(n)
+    #  @brief nまでのフィボナッチ計算をする
+    #  @param   n (int): 1つ目の引数
+    #  @return   int: fibonacci(n-1)+fibonacci(n-2), n==0のとき0, n==1のとき1
+
 
 def fibonacci(n):
     if n == 0:
@@ -11,4 +20,4 @@ def fibonacci(n):
     elif n == 1:
         return 1
     else:
-        return fibonacci(n - 1) + fibonacci(n - 2) 
+        return fibonacci(n - 1) + fibonacci(n - 2)
